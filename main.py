46b958ae1bc7664070f166d9a85a27d06ff75990
vanhuycx <vanhuycx@gmail.com>
2022-03-24 21:45:09 -0400

    Create sqlite3 connection and table to prepare for inserting data from the scraping

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
-url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 
-result = requests.get(url)
+try:
+    sqliteConnection = sqlite3.connect('SQLite_Python.db')
+    sqlite_create_table_query = '''CREATE TABLE SqliteDb_developers (
+                                    id INTEGER PRIMARY KEY,
+                                    name TEXT NOT NULL,
+                                    email text NOT NULL UNIQUE,
+                                    joining_date datetime,
+                                    salary REAL NOT NULL);'''
 
-doc = BeautifulSoup(result.text,'html.parser')
+    cursor = sqliteConnection.cursor()
+    print("Successfully Connected to SQLite")
+    cursor.execute(sqlite_create_table_query)
+    sqliteConnection.commit()
+    print("SQLite table created")
 
-print(doc.prettify())
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
+
+# for index in range(1,51):
+#     url = f'https://books.toscrape.com/catalogue/page-{index}.html'
+#     result = requests.get(url)
+#     doc = BeautifulSoup(result.text,'html.parser')
+#     olist = doc.find('section').find_all('div')[1].find('ol')
+#     for li_tag in olist.find_all('li'):
+#         book_title = li_tag.find('h3').string
+#         print(book_title)
+#         # print('----------------------------------------')
