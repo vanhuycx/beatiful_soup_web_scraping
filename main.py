df5c07e0ad11e4944b3393edd98cb6a0eb3ca2b0
vanhuycx <vanhuycx@gmail.com>
2022-03-24 23:04:31 -0400

    Script a books website and store the books' title in the sqlite3 db

--- main.py
+++ main.py
@@ -2,21 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-
 try:
     sqliteConnection = sqlite3.connect('SQLite_Python.db')
-    sqlite_create_table_query = '''CREATE TABLE SqliteDb_developers (
-                                    id INTEGER PRIMARY KEY,
-                                    name TEXT NOT NULL,
-                                    email text NOT NULL UNIQUE,
-                                    joining_date datetime,
-                                    salary REAL NOT NULL);'''
+    sqlite_create_table_query = '''CREATE TABLE Books (
+                                    title TEXT NOT NULL);'''
+
+    drop_table = 'drop table if exists Books'
 
     cursor = sqliteConnection.cursor()
     print("Successfully Connected to SQLite")
+
+    cursor.execute(drop_table)
+
     cursor.execute(sqlite_create_table_query)
     sqliteConnection.commit()
     print("SQLite table created")
+
+    for index in range(1,51):
+        url = f'https://books.toscrape.com/catalogue/page-{index}.html'
+        result = requests.get(url)
+        doc = BeautifulSoup(result.text,'html.parser')
+        li_list = doc.find('section').find_all('div')[1].find('ol').find_all('li')
+        for li_tag in li_list:
+            li_index = li_list.index(li_tag)
+
+            book_title = li_tag.find('h3').string
+            sqlite_insert_query = f'INSERT INTO Books (title)  VALUES  ({repr(f"{book_title}")})'
+
+            count = cursor.execute(sqlite_insert_query)
+            sqliteConnection.commit()
+            print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+            print(book_title)
+            print('----------------------------------------')
 
     cursor.close()
 
@@ -27,12 +44,4 @@ finally:
         sqliteConnection.close()
         print("The SQLite connection is closed")
 
-# for index in range(1,51):
-#     url = f'https://books.toscrape.com/catalogue/page-{index}.html'
-#     result = requests.get(url)
-#     doc = BeautifulSoup(result.text,'html.parser')
-#     olist = doc.find('section').find_all('div')[1].find('ol')
-#     for li_tag in olist.find_all('li'):
-#         book_title = li_tag.find('h3').string
-#         print(book_title)
-#         # print('----------------------------------------')
+
---
+++ test.py
@@ -0,0 +1,3 @@
+import re
+string = 'Shakespeare"s Sonnets'
+print(repr(f'{string}'))
