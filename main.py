85d8325c84bfab88f9d8c40d7e1133ffb23abcec
vanhuycx <vanhuycx@gmail.com>
2022-03-24 20:28:31 -0400

    Request URL and use htmlparser from bs4 to read

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
+
+result = requests.get(url)
+
+doc = BeautifulSoup(result.text,'html.parser')
+
+print(doc.prettify())
